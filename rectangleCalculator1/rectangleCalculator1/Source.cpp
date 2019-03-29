2b7741cb8f2483b911448c84550da2ef481a2a3d
kronbergr <46601276+kronbergr@users.noreply.github.com>
2019-03-28 20:49:24 -0400

    Add files via upload

---
+++ rectangleCalculator1/rectangleCalculator1/Source.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+	int length = 0;
+	int width = 0;
+	int area = 0;
+
+	cout << "\nInput length.";
+	cin >> length;
+
+	cout << "\nInput width.";
+	cin >> width;
+
+
+	area = length * width;
+
+	cout << "\nThe are of the rectangle is:" << area << endl;
+
+
+
+
+
+
+	system("Pause");
+	return 0;
+
+}
